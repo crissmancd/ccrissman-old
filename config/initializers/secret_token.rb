256b3c849337833309f672897bee832025ca8c2d
Chad Crissman <ccrissman@attinteractive.com>
2012-05-29 15:58:20 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ccrissman::Application.config.secret_token = '887e89522e445c5c70830ba9c6bd07fee7b3976934ecfa163f4fe8341e3f08c5c92bdd7eedd037db346214cccacea2ed578ed999a4c4e8c09af10d14eea324f2'
