c152b38d6e599538729185b7aaa3a962d356a129
Chad Crissman <cdcrissman@gmail.com>
2012-10-10 16:05:23 -0700

    moved samples into hashes

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -2,28 +2,28 @@ class SamplesController < ApplicationController
 
   def show
     samples = [
-      ["buzz-feed",        :png],
-      ["buzz-srp",         :jpg],
-      ["dueprops-profile", :jpg],
-      ["dueprops-signin",  :jpg],
-      ["rollcallme",       :jpg],
-      ["yp-hp",            :jpg],
-      ["yp-litehp",        :png],
-      ["yp-ypcrowd",       :jpg]
+      { slug: "buzz-feed",        extension: :png },
+      { slug: "buzz-srp",         extension: :jpg },
+      { slug: "dueprops-profile", extension: :jpg },
+      { slug: "dueprops-signin",  extension: :jpg },
+      { slug: "rollcallme",       extension: :jpg },
+      { slug: "yp-hp",            extension: :jpg },
+      { slug: "yp-litehp",        extension: :png },
+      { slug: "yp-ypcrowd",       extension: :jpg }
     ]
-    
-    @sample = samples.select{ |s| s.first == params[:slug] }.flatten
+
+    @sample = samples.select{ |s| s[:slug] == params[:slug] }.first
     sample_index = samples.index(@sample)
 
     if sample_index.zero?
       @prev_link = nil
-      @next_link = samples[sample_index + 1].first
+      @next_link = samples[sample_index + 1][:slug]
     elsif sample_index == (samples.length - 1)
-      @prev_link = samples[sample_index - 1].first
+      @prev_link = samples[sample_index - 1][:slug]
       @next_link = nil
     else
-      @prev_link = samples[sample_index - 1].first
-      @next_link = samples[sample_index + 1].first
+      @prev_link = samples[sample_index - 1][:slug]
+      @next_link = samples[sample_index + 1][:slug]
     end
   end
 
